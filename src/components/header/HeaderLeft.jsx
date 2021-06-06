529afa2689871cedfa61d37caf6e88a6134db300
vineet-coder <vineetsinghal2312@gmail.com>
2021-06-07 03:40:29 +0530

    initial commit

---
+++ src/components/header/HeaderLeft.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HeaderLeft = () => {
+  return <div className="bg-yellow-500 lg:w-1/4 w-1/2"></div>;
+};
---
+++ src/components/sideNav/SideNav.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SideNav = () => {
+  return <div className="side-nav h-screen w-1/4 bg-red-400">dwdw</div>;
+};
